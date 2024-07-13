1ae842158a074011fecf0e44652d661cf2c10b61
Fabian F <noobseca123@hotmail.com>
2024-07-12 19:52:03 -0500

    version 0.9

--- src/components/CartFlyout.jsx
+++ src/components/CartFlyout.jsx
@@ -18,24 +18,20 @@ export default function CartFlyout() {
   return $isProductOpen ? (
     <>
       <BlurWindow />
-      <aside className="w-56 md:w-80 h-screen border-l-4 fixed top-0 right-0 bg-red-100 z-40 py-4 px-2">
-        <CartFlyoutToggle />
-        {Object.values($cartProducts).length ? (
-          <ul className="space-y-1">
-            {Object.values($cartProducts).filter(cartProduct => parseInt(cartProduct.quantity, 10) > 0).map((cartProduct) => (
-              <li className="flex items-center justify-around border-2 rounded-xl bg-white h-32 p-2 " key={cartProduct.id}>
-                <img className="w-20 md:w-36 " src={cartProduct.imageSrc} alt={cartProduct.name} />
-                <div><h3 className="font-script text-lg">{cartProduct.name}</h3>
-                  <p className="text-sm ">Unit Value: <span className="text-md font-semibold">${cartProduct.price}</span></p>
-                  <p className="text-sm ">Cantidad: <span className="text-md font-semibold">{cartProduct.quantity}</span></p>
-                  <div >
-                    <AddToCartProduct id={cartProduct.name} name={cartProduct.name} imageSrc={cartProduct.imageSrc} price={cartProduct.price}   >
-                      <button type="submit"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-                      </svg>
-                      </button>
-                    </AddToCartProduct>
+      <aside  className="w-60 md:w-80 h-screen border-l-4 fixed top-0 right-0 bg-white z-40">
+        <div className="flex items-center justify-between bg-teal-400 p-4">
+          <button><CartFlyoutToggle /></button>
+          <span className="text-white font-bold text-xl">COP $---</span>
+        </div>
 
+        {Object.values($cartProducts).length ? (
+          <ul className="space-y-1 p-2 border-b-4 ">
+            {Object.values($cartProducts).filter(cartProduct => parseInt(cartProduct.quantity, 10) > 0).map((cartProduct) => (
+              <li className=" border-gray-400 rounded-xl bg-white h-28 md:h-32 py-2 px-2 md:px-4  border-2 " key={cartProduct.id}>
+                <h3 className="font-script text-lg text-center">{cartProduct.name}</h3>
+                <div className="mt-2 flex items-center justify-between ">
+                  <img className="w-16 md:w-20 " src={cartProduct.imageSrc} alt={cartProduct.name} />
+                  <div className="flex items-center justify-between space-x-1 md:space-x-4 ">
                     <RemoveToCartProduct
                       id={cartProduct.name}
                       name={cartProduct.name}
@@ -59,9 +55,18 @@ export default function CartFlyout() {
                         </svg>
                       </button>
                     </RemoveToCartProduct>
-                  </div>
-                </div>
+                    <span className="text-lg font-semibold">{cartProduct.quantity}</span>
+                    <AddToCartProduct id={cartProduct.name} name={cartProduct.name} imageSrc={cartProduct.imageSrc} price={cartProduct.price}  >
+                      <button type="submit"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                      </svg>
+                      </button>
+                    </AddToCartProduct>
 
+                  </div>
+                  <p className="text-center "> <span className="text-md font-semibold">${parseInt(cartProduct.price, 10) * cartProduct.quantity},00</span></p>
+
+                </div>
               </li>
             ))}
             {Object.values($cartProducts).some(cartProduct => parseInt(cartProduct.quantity, 10) > 0) ? (
